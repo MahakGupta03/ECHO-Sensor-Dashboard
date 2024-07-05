2f09202360b6d7d273138748191b3c5af18b3ccd
MahakGupta03 <98971021+MahakGupta03@users.noreply.github.com>
2024-07-05 22:57:33 +0530

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -7,9 +7,12 @@ from django.contrib import messages
 import requests
 from .models import Profile  # import the Profile model
 from emails.models import Gases
+import time
 
 # Create your views here.
 def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         first_name=request.POST['first_name']
         last_name=request.POST['last_name']
@@ -37,8 +40,8 @@ def register(request):
         print("User created successfully..")
         messages.success(request, "User created successfully..")
 
-
-        return HttpResponseRedirect(request.path_info)
+        return redirect('login')
+        # return HttpResponseRedirect(request.path_info)
     return render(request,'register.html')
 
 def user_login(request):
@@ -58,8 +61,13 @@ def user_login(request):
 
         if user_obj :
             login(request, user_obj)
-            return redirect('dashboard')
+            # if email == 'yash@gmail.com' and password == 'yash':
+            #     return redirect('home')
+                # return redirect('admin-dashboard')
+            # return redirect('dashboard')
+
             # return render(request,'dashboard.html', {'user':user_obj, 'profile':u[0]})
+            return redirect('home')
         
         messages.warning(request, "Invalid credentials..")
         return HttpResponseRedirect(request.path_info)
@@ -76,13 +84,16 @@ def dashboard(request):
     email = 'yashhguptaa.917@gmail.com'
 
     all_data = Gases.objects.all()
-    result = requests.get("https://api.thingspeak.com/channels/2509489/feeds.json?api_key=246K3ARFKVT42S7O&results=7").json()
+    # result = requests.get("https://api.thingspeak.com/channels/2509489/feeds.json?api_key=246K3ARFKVT42S7O&results=7").json()
+    result = requests.get("https://api.thingspeak.com/channels/2509489/feeds.json?api_key=246K3ARFKVT42S7O&results=7&timezone=Asia/Kolkata").json()
     if not request.user.is_authenticated:
         messages.warning(request, "Login required")
         return render(request,'login.html')
     u = Profile.objects.filter(user = request.user)
         # list = []
     
+    
+
     hydrogensulfide_list = []
     # carbondioxide_list = []
     # ammonia_list = []
@@ -119,13 +130,36 @@ def dashboard(request):
         'profile':u[0],
 
     }
-    if hydrogensulfide_list[-1] > 35:
-        messages.warning(request, "Danger ahead. Level is more than 35")
+    if hydrogensulfide_list[-1] > 60:
+        messages.warning(request, "Danger ahead. Level is more than 60 ppm")
 
 
     return render(request,"dashboard.html", context)
     # return render(request,'dashboard.html', {'user':request.user, 'profile':u[0]})
 
+def home(request):
+    if not request.user.is_authenticated:
+        messages.warning(request, "Login required")
+        return render(request,'login.html')
+    users = User.objects.all()
+    return render(request,"home.html", {'users':users})
+
+def zone_list(request):
+    if not request.user.is_authenticated:
+        messages.warning(request, "Login required")
+        return render(request,'login.html')
+    # users = User.objects.all()
+    return render(request,"zonelist.html")
+
+def admin_dashboard(request):
+    if not request.user.is_authenticated:
+        messages.warning(request, "Login required")
+        return render(request,'login.html')
+    users = User.objects.all()
+    return render(request,"admindashboard.html", {'users':users})
+
 def show_city_plan(request):
-    
+    if not request.user.is_authenticated:
+        messages.warning(request, "Login required")
+        return render(request,'login.html')
     return render(request,"cityplan.html")
--- emails/urls.py
+++ emails/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/home',views.home, name="home"),
+    # path('/home',views.home, name="home"),
     path('/user-dashboard',views.sent_threshold_mail,name="user-dashboard")
 ]
